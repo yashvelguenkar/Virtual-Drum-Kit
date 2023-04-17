522c74fb856d773e186b1e4e3e0339d8d19784ec
yashvelguenkar <129824128+yashvelguenkar@users.noreply.github.com>
2023-04-17 09:56:11 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,77 @@
+
+for( var i =0; i<document.querySelectorAll(".drum").length; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+ 
+    var letterOfDrums = this.innerHTML;
+    makeSound(letterOfDrums);
+    buttonAnimation(letterOfDrums);
+    
+    });
+}
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+   
+});
+
+function makeSound(key){
+
+    switch(key) {
+
+        case "w": 
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        
+        case "a":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+
+            case "s":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                break;
+                    
+            case "d":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                break;
+                                    
+            case "j":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+
+                case "k":
+                    var audio = new Audio('sounds/tom-3.mp3');
+                    audio.play();
+                    break;
+
+                case "l":
+                    var audio = new Audio('sounds/tom-4.mp3');
+                    audio.play();
+                    break;
+                
+                default: console.log("baba re baba")
+        }
+
+}
+
+function buttonAnimation(currentKey){
+    var activeKey = document.querySelector("." + currentKey);
+
+    activeKey.classList.add("pressed");
+
+    setTimeout( function() { activeKey.classList.remove("pressed"), 200});
+
+
+
+}
